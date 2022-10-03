dbaea1e4e1fcf0bd51d1a05018c14e148424bed5
Emeka Rex Chukwu <81708052+Emeka-web-dev@users.noreply.github.com>
2022-10-03 17:42:07 +0100

    Update sanity.ts

--- sanity.ts
+++ sanity.ts
@@ -3,9 +3,9 @@ import createImageUrlBuilder from "@sanity/image-url";
 
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: "m7s3usfm",
   apiVersion: "2021-10-21",
-  useCdn: process.env.NODE_ENV || "production",
+  useCdn: process.env.NODE_ENV === "production",
 };
 
 export const sanityClient = createClient(config);
