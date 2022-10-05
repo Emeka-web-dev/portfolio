18bbecda0baf166786d20ae933a367b79cfe0a3d
Emeka-web-dev <emekarexchukwu@gmail.com>
2022-10-05 15:40:19 +0100

    trying commit

--- sanity.ts
+++ sanity.ts
@@ -2,10 +2,10 @@ import { createClient } from "next-sanity";
 import createImageUrlBuilder from "@sanity/image-url";
 
 export const config = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  dataset: "production",
   projectId: "m7s3usfm",
   apiVersion: "2021-10-21",
-  useCdn: process.env.NODE_ENV === "production",
+  useCdn: false,
 };
 
 export const sanityClient = createClient(config);
